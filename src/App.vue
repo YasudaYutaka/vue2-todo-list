288d5d410ecc1b601d7e292f41b853dc51c6cc5e
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-01 11:27:21 -0300

    List component

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,35 @@
   <v-app>
     <h2 class="text-center mt-10">Todo<span class="font-weight-thin">List</span></h2>
     <v-main>
-      <transition name="slide" type="animation" appear>
-        <Input />
-      </transition>
+      <transition-group name="slide" type="animation" appear>
+        <Input key='input' :todo="todos"/>
+        <List key='list' :todos="todos" :deleteTodo="deleteTodo"/>
+      </transition-group>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Input from './components/Input.vue';
+import List from './components/List.vue';
 
 export default {
   name: 'App',
 
   components: {
-    Input
+    Input,
+    List
   },
 
   data: () => ({
-    //
+    todos: [],
   }),
+
+  methods: {
+    deleteTodo(index) {
+      this.todos.splice(index, 1)
+    }
+  }
 };
 </script>
 
@@ -40,18 +49,10 @@ export default {
 
   @keyframes slide-in {
     from {
-        transform: translateY(20px);
-    }
-    to {
-      transform: translateY(0);
-    }
-  }
-  @keyframes slide-out {
-    from {
-      transform: translateY(0);
-    }
-    to {
       transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
     }
   }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-container class="my-6 container" max-width="60vw">
+        <hr/>
+        <ul class="my-5">
+            <transition-group name="slide" type="transition">
+                <v-card class="my-5 pa-5 d-flex flex-row justify-space-between" v-for="(todo, index) in todos" :key="index">
+                    <li>{{todo}}</li>
+                    <v-btn class="deep-purple darken-4" dark small @click="deleteTodo(index)">Remove</v-btn>
+                </v-card>
+            </transition-group>
+        </ul>
+    </v-container>
+</template>
+
+<script>
+export default {
+    props: ['todos', 'deleteTodo'],
+}
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+    }
+
+    @media (min-width: 1193px) {
+        .container {
+            max-width: 40vw !important;
+        }
+    }
+
+  .slide-enter {
+    opacity: 0;
+  }
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+  }
+  .slide-leave-active {
+    transition: opacity .5s;
+    opacity: 0;
+
+  }
+  .slide-move {
+    transition: transition 1s;
+  }
+  @keyframes slide-in {
+    from {
+        transform: translateY(20px);
+    }
+    to {
+        transform: translateY(0);
+    }
+  }
+  @keyframes slide-out {
+    from {
+        transform: translateY(0);
+    }
+    to {
+        transform: translateY(20px);
+    }
+  }
+</style>
