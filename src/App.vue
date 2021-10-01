ba7507d590e27963d4ea5b4106e50035811c2f9d
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-01 10:45:23 -0300

    slide transition

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <v-app>
     <h2 class="text-center mt-10">Todo<span class="font-weight-thin">List</span></h2>
     <v-main>
-      <Input />
+      <transition name="slide" type="animation" appear>
+        <Input />
+      </transition>
     </v-main>
   </v-app>
 </template>
@@ -22,3 +24,34 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+  .slide-enter {
+    opacity: 0;
+  }
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+  }
+  .slide-leave-active {
+    transition: opacity .5s;
+    opacity: 0;
+  }
+
+  @keyframes slide-in {
+    from {
+        transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
+</style>
